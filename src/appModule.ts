a1443afa40c3e41e4b5fcd4c9cec230aeec1f23f
victorialucena <lucenavictoria0@gmail.com>
2024-09-13 12:29:54 -0300

    feat: :lock: add dotenv

--- src/appModule.ts
+++ src/appModule.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CustomerModule } from './modules/customerModule';
 import { AccountModule } from './modules/accountModule';
 import { ManagerModule } from './modules/managerModule';
@@ -7,7 +9,6 @@ import { AccountController } from './application/controllers/accountController';
 import { ManagerController } from './application/controllers/managerController';
 import { PaymentModule } from './modules/payment.module';
 import { PaymentController } from './application/controllers/payment.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { Manager } from './domain/entities/entiteManager';
 import { Customer } from './domain/entities/entitieCustomer';
 import { Account } from './domain/entities/entitieAccount';
@@ -17,17 +18,37 @@ import { TransactionModule } from './modules/transactionModule';
 import { TransactionController } from './application/controllers/transactionController';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: 'postgres',
-    host: 'localhost',
-    port: 5433,
-    database: 'postgres',
-    username: 'postgres',
-    password: '#Helo2019',
-    entities: [Manager, Customer, Account, Payment, Transaction],
-    synchronize: true,
-  }), CustomerModule, AccountModule, ManagerModule, PaymentModule, TransactionModule],
-  controllers: [CustomerController, AccountController, ManagerController, PaymentController, TransactionController],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, 
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        entities: [Manager, Customer, Account, Payment, Transaction],
+        synchronize: true,
+      }),
+    }),
+    CustomerModule,
+    AccountModule,
+    ManagerModule,
+    PaymentModule,
+    TransactionModule,
+  ],
+  controllers: [
+    CustomerController,
+    AccountController,
+    ManagerController,
+    PaymentController,
+    TransactionController,
+  ],
   providers: [],
 })
-export class AppModule { }
+export class AppModule {}
